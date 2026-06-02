0e03ccb44f9471e64403a14589063e0f50ad83bc
Brandon Kessinger <Brandon@CardHabitApp.com>
2026-06-01 22:25:54 -0700

    Fix GitHub username (brandonkessinger -> Bkessing) in URLs

--- bin/aegis-sb.js
+++ bin/aegis-sb.js
@@ -5,7 +5,7 @@ console.log('');
 console.log('Real functionality lands in v0.1. This release reserves the');
 console.log('package name and lays the groundwork.');
 console.log('');
-console.log('Roadmap + status: https://github.com/brandonkessinger/aegis-sb');
+console.log('Roadmap + status: https://github.com/Bkessing/aegis-sb');
 console.log('Follow along:    https://x.com/KessingerBuilds');
 console.log('');
 process.exit(0);
